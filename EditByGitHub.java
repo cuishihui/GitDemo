accc26cfa1354063867fcde489bff832d79208a3
cuishihui <601958496@qq.com>
2020-02-19 15:32:39 +0800

    我又下载下来,又更新了一下

--- EditByGitHub.java
+++
@@ -1,8 +0,0 @@
-public EditByGitHub{
-
-public static void main(){
-System.out.println("你好我是在GitHub上写的类");
-
-}
-
-}
---
+++ domain/src/main/java/EditByGitHub.java
@@ -0,0 +1,6 @@
+public class EditByGitHub{
+
+    public static void main(String[] args) {
+        System.out.println("你好我是在GitHub上写的类");
+    }
+}
