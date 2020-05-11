0354450f648b55187ee1dfc4eac17372da96d28c
Cuishihui <601958496@qq.com>
2020-05-11 21:27:36 +0800

    崔世辉1

--- domain/src/main/java/EditByGitHub.java
+++ domain/src/main/java/EditByGitHub.java
@@ -4,5 +4,6 @@ public class EditByGitHub{
         System.out.println("你好我是在GitHub上写的类");
         System.out.println("我是在第二个demo1中修改的");
         System.out.println("你看我哈拉少吗?");
+        System.out.println("我是GitDemo里面第二次修改的数据");
     }
 }
