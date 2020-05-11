7a03361eef37539503765dc8af3d6ec8e165d59c
Cuishihui <601958496@qq.com>
2020-05-11 21:15:37 +0800

    111

--- domain/src/main/java/EditByGitHub.java
+++ domain/src/main/java/EditByGitHub.java
@@ -2,6 +2,7 @@ public class EditByGitHub{
 
     public static void main(String[] args) {
         System.out.println("你好我是在GitHub上写的类");
+        System.out.println("我是在第二个demo1中修改的");
         System.out.println("你看我哈拉少吗?");
     }
 }
